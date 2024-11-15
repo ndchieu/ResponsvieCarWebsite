cd0825ca9b90f1d7a7d8cbb1d84dbe41350cd9c1
ndchieu <mylover21072000@gmail.com>
2024-11-15 11:47:43 +0700

    update source

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,11 +59,75 @@ var swiper = new Swiper(".popular_container", {
   },
 });
 /*=============== MIXITUP FILTER FEATURED ===============*/
-
+let mixerFeatured = mixitup(".featured_content", {
+  selectors: {
+    target: ".featured_card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 /* Link active featured */
+
+const linkFeatured = document.querySelectorAll(".featured_item");
+
+function activeFeatured() {
+  linkFeatured.forEach((l) => l.classList.remove("active_featured"));
+  this.classList.add("active_featured");
+}
+linkFeatured.forEach((l) => l.addEventListener("click", activeFeatured));
 
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav_menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".home_title, .popular_container, .features_img, .featured_filters");
+sr.reveal(".home_subtitle", { delay: 500 });
+sr.reveal(".home_elec", { delay: 600 });
+sr.reveal(".home_img", { delay: 800 });
+sr.reveal(".home_car-data", { delay: 900, interval: 100, origin: "bottom" });
+sr.reveal(".home_button", { delay: 1000, origin: "bottom" });
+sr.reveal(".about_group, .offer_data", { origin: "left" });
+sr.reveal(".about_data", { origin: "right" });
+sr.reveal(".features_map, .offer_img", { delay: 600, origin: "right" });
+sr.reveal(".features_card", { interval: 300 });
+sr.reveal(".featured_card, .logos_content, .footer_content", { interval: 100 });
